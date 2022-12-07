95ac14476cfe167e88545b344ce9195fc7d6347d
Sena Sücüllü <senaamervee@gmail.com>
2022-12-08 02:15:09 +0300

    login-devam1

--- src/login.js
+++ src/login.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View,Image } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons'; 
+
 
 
 export default function login() {
@@ -10,33 +12,41 @@ export default function login() {
         />
         
         
+        <View style={styles.main}>
         <View style={styles.textınput}>
-        <View>
-        <TextInput style={{fontSize:12,fontWeight:'200'}}>Username or email address</TextInput>
+        <TextInput style={{fontSize:12,fontWeight:'300'}} 
+        placeholder='Username or email address' />
         </View>
         </View>
      
      
-     <View style={styles.textınput}>
-        <View>
-        <TextInput style={{fontSize:12,fontWeight:'200'}}>Password</TextInput>
+     <View style={styles.main}>
+        <View style={styles.textınput}>
+        <TextInput style={{fontSize:12,fontWeight:'300'}}
+        placeholder='Password'
+       />
         </View>
       </View>
      <TouchableOpacity>
       <View style={styles.logın}>
         <View style={styles.main}>
-        <Text style={styles.text}>Log In</Text>
+        <Text style={styles.text}>Sign In </Text>
         </View>
       </View>
       </TouchableOpacity>
       <View>
       <TouchableOpacity>
-      <View style={styles.signup}>
-        <View style={styles.main}>
-        <Text style={styles.text}>Sign Up</Text>
+        <View style={{marginTop:15,alignItems:'center'}}>
+          <Text style={{fontSize:12,fontWeight:'300'}}>Forgot Password?</Text>
         </View>
-      </View>
       </TouchableOpacity>
+      </View>
+      <View style={{flexDirection:'row',justifyContent:'space-between',backgroundColor:'pink'}}>
+        <View >
+        <AntDesign name="googleplus" size={24} color="black" />
+        <Text>Sign In with Google </Text>
+        </View>
+
       </View>
     </View>
   )
@@ -44,8 +54,8 @@ export default function login() {
 
 const styles = StyleSheet.create({
   textınput:{
-    height:29,
-    width:242,
+    
+    width:'100%',
     backgroundColor:'#E4E4E4',
     alignContent:'center',
     justifyContent:'center',
@@ -55,12 +65,12 @@ const styles = StyleSheet.create({
   },
   logın:{
     marginTop:30,
-    height:29,
-    width:242,
+    width:'100%',
     backgroundColor:'#C13061',
     alignContent:'center',
     justifyContent:'center',
     borderRadius:5,
+    padding:2,
 
 
   },
@@ -72,20 +82,20 @@ const styles = StyleSheet.create({
   main:{
     justifyContent:'center',
     alignItems:'center',
-  },
-  signup:{
-    height:29,
-    width:242,
-    backgroundColor:'#ABABAB',
-    alignContent:'center',
-    justifyContent:'center',
-    borderRadius:5,
-    marginTop:10,
 
   },
   ımage:{
     width:253,
     height:158,
+  },
+  google:{
+    marginTop:30,
+    width:'100%',
+    backgroundColor:'rgba(236, 236, 236, 0.5)',
+    borderRadius:5,
+    padding:2,
+    flexDirection:'row',   
+
   }
   
 })
